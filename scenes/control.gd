ba3783beffb91ceff247b8ef0e45ef40b888c4c1
agustinmoralesmillan-cpu <agustin.moralesmillan@escuelacangallo.edu.ar>
2026-08-05 10:38:38 -0300

    puse puntuaciones al azar con nombres de gente al azar para tener maso menos una idea de como queres tenerlo

--- scenes/control.gd
+++ scenes/control.gd
@@ -4,7 +4,7 @@ extends Control
 
 
 func _on_jugar_pressed():
-	get_tree().change_scene_to_file("res://scenes/el_mundo_test.tscn")
+	get_tree().change_scene_to_file("res://scenes/working_road_example.tscn")
 
 
 func _on_opciones_pressed():
